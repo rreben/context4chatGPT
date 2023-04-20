b46ce8720c965e82de3ecf00e7817022823df6e9
rreben <rupert.rebentisch@gmail.com>
2023-04-20 20:22:24 +0200

    exception handling and spinning wheel

--- context4chatGPT.py
+++ context4chatGPT.py
@@ -1,7 +1,25 @@
+import openai
 from llama_index import GPTSimpleVectorIndex, SimpleDirectoryReader
 import os
 import sys
 import click
+import time
+import threading
+
+
+def spinning_cursor():
+    while True:
+        for cursor in '|/-\\':
+            yield cursor
+
+
+def spinner(stop_spinner):
+    spin = spinning_cursor()
+    while not stop_spinner.is_set():
+        sys.stdout.write(next(spin))
+        sys.stdout.flush()
+        time.sleep(0.1)
+        sys.stdout.write('\b')
 
 
 def check_api_key():
@@ -29,19 +47,40 @@ def main(input_folder, recreate_index):
     question = ' '
     check_api_key()
     if recreate_index or not os.path.exists('index.json'):
-        loader = SimpleDirectoryReader(input_folder)
-        documents = loader.load_data()
-        index = GPTSimpleVectorIndex(documents)
-        index.save_to_disk('index.json')
-        click.echo(
-            f'Successfully processed {len(os.listdir(input_folder))} files.')
+        stop_spinner = threading.Event()
+        spinner_thread = threading.Thread(
+            target=spinner, args=(stop_spinner,), daemon=True)
+        print("Indexing files. This may take a while...")
+        spinner_thread.start()
+        try:
+            loader = SimpleDirectoryReader(input_folder)
+            documents = loader.load_data()
+            index = GPTSimpleVectorIndex(documents)
+            index.save_to_disk('index.json')
+            click.echo(
+                'Successfully processed' +
+                f'{len(os.listdir(input_folder))} files.')
+        finally:
+            stop_spinner.set()
+            spinner_thread.join()
+            print("\nIndex creation completed.")
     else:
         index = GPTSimpleVectorIndex.load_from_disk('index.json')
     while question:
         question = input("Type a question :")
         if question != '':
-            response = index.query(question)
-            print(response)
+            try:
+                response = index.query(question)
+                print(response)
+            except openai.api_errors.RateLimitError as e:
+                print(f"Quota exceeded. Please wait and try again later. {e}")
+                # Optional: Implement a retry mechanism or logging here
+            except openai.api_errors.APIError as e:
+                print(f"An API error occurred: {e}")
+                # Optional: Handle other API errors here
+            except Exception as e:
+                print(f"An unexpected error occurred: {e}")
+                # Optional: Handle other unexpected errors here
         else:
             break
 
